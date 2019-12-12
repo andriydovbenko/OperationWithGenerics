ef7cb9446cbd63e0646e603b0dbb5b0290c98967
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-12 17:21:14 +0100

    deleted redundant filds

--- src/com/cursor/generics/Main.java
+++ src/com/cursor/generics/Main.java
@@ -3,13 +3,11 @@ package com.cursor.generics;
 import com.cursor.generics.task1.ExecutionTask1;
 import com.cursor.generics.task2.ExecutionTask2;
 
-
 public class Main {
 
     public static void main(String[] args) {
         ExecutionTask1.findLargestSmallest();
 
         ExecutionTask2.startProgram();
-
     }
 }
