ee3e97a909919ca02c993f34802e62ed1cb463a7
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-12 17:18:34 +0100

    inin Main.java

--- src/com/cursor/generics/Main.java
+++ src/com/cursor/generics/Main.java
@@ -1,10 +1,15 @@
 package com.cursor.generics;
 
-import com.cursor.generics.task1.Generics;
+import com.cursor.generics.task1.ExecutionTask1;
+import com.cursor.generics.task2.ExecutionTask2;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        Generics.findLargestSmallest();
+        ExecutionTask1.findLargestSmallest();
+
+        ExecutionTask2.startProgram();
+
     }
 }
