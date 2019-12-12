97f1d00fe0f60c4e279eaa711ab09e4bce94ef50
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-12 17:15:52 +0100

    renamed Class

--- src/com/cursor/generics/task1/ExecutionTask1.java
+++ src/com/cursor/generics/task1/ExecutionTask1.java
@@ -1,6 +1,6 @@
 package com.cursor.generics.task1;
 
-public class Generics {
+public class ExecutionTask1 {
 
     public static void findLargestSmallest() {
         float[] arrayOfFloat = {20, 11.0f, 0f, -1f, 60.055f, 18f, 88.440f, 11.05f};
@@ -8,7 +8,7 @@ public class Generics {
         for (Float digit : arrayOfFloat) {
             listOfFloat.add(digit);
         }
-        System.out.println("The largest digit of list is: " + listOfFloat.largest() +
-                "\nThe smallest digit of list is: " + listOfFloat.smallest());
+        System.out.println("\nThe largest digit in list is: " + listOfFloat.largest() +
+                "\nThe smallest digit in list is: " + listOfFloat.smallest());
     }
 }
--- src/com/cursor/generics/task1/MyList.java
+++ src/com/cursor/generics/task1/MyList.java
@@ -3,29 +3,34 @@ package com.cursor.generics.task1;
 import java.util.ArrayList;
 
 public class MyList<T extends Number> {
-    ArrayList<T> listOfNumbers = new ArrayList<>();
-
-    public void add(T addNumber) {
-        listOfNumbers.add(addNumber);
+    private ArrayList<T> listOfNumbers = new ArrayList<>();
+    public void add(T parameter) {
+        listOfNumbers.add(parameter);
     }
 
     public T largest() {
-        T largestValue = listOfNumbers.get(0);
-        for (int i = 1; i < listOfNumbers.size(); i++) {
-            if (largestValue.doubleValue() < listOfNumbers.get(i).doubleValue()) {
-                largestValue = listOfNumbers.get(i);
+        if (listOfNumbers.size() > 0) {
+            T largestValue = listOfNumbers.get(0);
+            for (int i = 1; i < listOfNumbers.size(); i++) {
+                if (largestValue.doubleValue() < listOfNumbers.get(i).doubleValue()) {
+                    largestValue = listOfNumbers.get(i);
+                }
             }
+            return largestValue;
         }
-        return largestValue;
+        return null;
     }
 
     public T smallest() {
-        T smallestValue = listOfNumbers.get(0);
-        for (int i = 1; i < listOfNumbers.size(); i++) {
-            if (smallestValue.doubleValue() > listOfNumbers.get(i).doubleValue()) {
-                smallestValue = listOfNumbers.get(i);
+        if (listOfNumbers.size() > 0) {
+            T smallestValue = listOfNumbers.get(0);
+            for (int i = 1; i < listOfNumbers.size(); i++) {
+                if (smallestValue.doubleValue() > listOfNumbers.get(i).doubleValue()) {
+                    smallestValue = listOfNumbers.get(i);
+                }
             }
+            return smallestValue;
         }
-        return smallestValue;
+        return null;
     }
 }
