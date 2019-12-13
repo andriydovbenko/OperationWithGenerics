e5a5fcc116de2f44cc5b4d41b71f2b0615d66607
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-13 09:31:52 +0100

    init ExecutionProjext.java

---
+++ src/com/cursor/generics/ExecutionProject.java
@@ -0,0 +1,12 @@
+package com.cursor.generics;
+
+import com.cursor.generics.task1.ExecutionTask1;
+import com.cursor.generics.task2.ExecutionTask2;
+
+public class ExecutionProject {
+
+    public static void start() {
+        ExecutionTask1.findLargestSmallest();
+        ExecutionTask2.startProgram();
+    }
+}
