e72d025246f06d1a0668b7834fa6f0d38e257639
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-16 11:03:40 +0100

    extended type of class

--- src/com/cursor/generics/task1/MyList.java
+++ src/com/cursor/generics/task1/MyList.java
@@ -1,38 +1,11 @@
 package com.cursor.generics.task1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class MyList<T extends Number> {
+public class MyList<T extends Number & Comparable<? super T>> {
     private List<T> listOfNumbers = new ArrayList<>();
 
-    public void add(T parameter) {
-        listOfNumbers.add(parameter);
-    }
 
-    public T findLargestValue() {
-        T largestValue = null;
-        if (listOfNumbers.size() > 0) {
-            largestValue = listOfNumbers.get(0);
-            for (int i = 1; i < listOfNumbers.size(); i++) {
-                if (largestValue.doubleValue() < listOfNumbers.get(i).doubleValue()) {
-                    largestValue = listOfNumbers.get(i);
-                }
-            }
-        }
-        return largestValue;
-    }
-
-    public T findSmallestValue() {
-        T smallestValue = null;
-        if (listOfNumbers.size() > 0) {
-            smallestValue = listOfNumbers.get(0);
-            for (int i = 1; i < listOfNumbers.size(); i++) {
-                if (smallestValue.doubleValue() > listOfNumbers.get(i).doubleValue()) {
-                    smallestValue = listOfNumbers.get(i);
-                }
-            }
-        }
-        return smallestValue;
-    }
 }
