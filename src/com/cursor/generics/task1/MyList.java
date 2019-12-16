a175363537761d18440a99add7301623abcd6aea
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-16 11:04:34 +0100

    added constructor and methods to class

--- src/com/cursor/generics/task1/MyList.java
+++ src/com/cursor/generics/task1/MyList.java
@@ -7,5 +7,15 @@ import java.util.List;
 public class MyList<T extends Number & Comparable<? super T>> {
     private List<T> listOfNumbers = new ArrayList<>();
 
+    public void add(T parameter) {
+        listOfNumbers.add(parameter);
+    }
 
+    public T findLargestValue() {
+        return listOfNumbers.isEmpty() ? null : Collections.max(listOfNumbers);
+    }
+
+    public T findSmallestValue() {
+        return listOfNumbers.isEmpty() ? null : Collections.min(listOfNumbers);
+    }
 }
